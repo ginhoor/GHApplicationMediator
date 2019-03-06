3b9ec23729d5d5294ddad31902384f19e4e7ee0d
Ginhoor <ginhoor@gmail.com>
2019-03-06 16:53:22 +0800

    init

---
+++ ApplicationMediator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ApplicationMediator
+//
+//  Created by JunhuaShao on 2019/3/6.
+//  Copyright © 2019 JunhuaShao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
